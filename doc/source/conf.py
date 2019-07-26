2540f858cdcfe0d00740c809c26bc43e01a8ba82
pengyuesheng <pengyuesheng@gohighsec.com>
2019-07-26 11:25:43 +0800

    Bump the openstackdocstheme extension to 1.20
    Some options are now automatically configured by the version 1.20:
    - project
    - html_last_updated_fmt
    - latex_engine
    - latex_elements
    - version
    - release.
    
    Change-Id: Iacccbabe71f3b68bdf28ecc349571887f191a3cd

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -26,7 +26,6 @@ extensions = [
 repository_name = 'openstack/os-vif'
 bug_project = 'os-vif'
 bug_tag = ''
-html_last_updated_fmt = '%Y-%m-%d %H:%M'
 
 # The suffix of source filenames.
 source_suffix = '.rst'
@@ -35,7 +34,6 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'os-vif'
 copyright = u'2016, OpenStack Foundation'
 
 # If true, '()' will be appended to :func: etc. cross-reference text.
--- releasenotes/source/conf.py
+++ releasenotes/source/conf.py
@@ -30,7 +30,6 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'os-vif release Notes'
 copyright = u'2017, OpenStack Foundation'
 
 # Release notes do not need a version in the title, they span

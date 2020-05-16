94f17912c54ea73f94c16c058dc7e663db2fdd14
Andreas Jaeger <aj@suse.com>
2020-05-16 09:31:14 +0200

    Switch to newer openstackdocstheme and reno versions
    Switch to openstackdocstheme 2.2.1 and reno 3.1.0 versions. Using
    these versions will allow especially:
    * Linking from HTML to PDF document
    * Allow parallel building of documents
    * Fix some rendering
    
    Update Sphinx version as well.
    
    openstackdocstheme renames some variables, so follow the renames
    before the next release removes them. A couple of variables are also
    not needed anymore, remove them.
    
    Change pygments_style to 'native' since old theme version always used
    'native' and the theme now respects the setting and using 'sphinx' can
    lead to some strange rendering.
    
    See also
    http://lists.openstack.org/pipermail/openstack-discuss/2020-May/014971.html
    
    Change-Id: I612722e75975d6cfd5fc61037834b675117019e7

--- releasenotes/source/conf.py
+++ releasenotes/source/conf.py
@@ -40,7 +40,7 @@ release = ''
 version = ''
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = 'native'
 
 
 # -- Options for HTML output ----------------------------------------------
